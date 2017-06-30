68642927e8bf10d91bd58f6e2540f4440cd73e6c
Gabriela Parra <parragabriela@icloud.com>
2017-06-30 12:41:56 -0400

    Redirected users trying to access resources only visible to logged in users

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+//only for logged in users
+router.get('/special', (req, res, next) => {
+  if (req.user) {
+    res.render('special-secret-view.ejs');
+  } else {
+    res.redirect('/login');
+  }
+});
+
 module.exports = router;
